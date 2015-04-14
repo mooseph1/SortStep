6c34facf1b8ceea934006bae580202343e72c9ea
unknown <jokim4@697-334-Edoras.sf.k12.internal>
2015-04-14 12:15:22 -0700

    done

--- build-tmp/source/SortStep.java
+++ build-tmp/source/SortStep.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class SortStep extends PApplet {
+
 private int numInts = 8000;
 private int largestInt = 100;
 private int [] myArray= new int[numInts];
@@ -61,3 +77,12 @@ private void fillArray(int numInts, int largestInt)
   }
 }
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "SortStep" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
